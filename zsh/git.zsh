38f9c418bb2a93e4bbb4f6fd75cf49da8c621065
AVONSTU1 <AVONSTU1@ford.com>
2023-03-23 18:54:16 -0400

    git config aliases

---
+++ zsh/git.zsh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+git config --global alias.st status
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.cm commit
+git config --global alias.df diff
+git config --global alias.lg "log --oneline --decorate --all --graph"
+git config --global alias.cp cherry-pick
+git config --global alias.rb rebase
+git config --global alias.mrg merge
