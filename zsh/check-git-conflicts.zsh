098f515e6b359f17e588bd116f536236b29956c4
AVONSTU1 <AVONSTU1@ford.com>
2023-03-31 14:21:26 -0400

    run conflict check only with internet connection

--- zsh/check-git-conflicts.zsh
+++ zsh/check-git-conflicts.zsh
@@ -5,30 +5,44 @@ CONFIG_REPO_PATH="$HOME/.config"
 MAIN_BRANCH="main"
 CONFLICT_THRESHOLD=5
 
+# Function to check internet connectivity
+function check_internet() {
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    ping -q -c 1 -W 1 8.8.8.8 > /dev/null 2>&1
+  else
+    # Termux (Android) or other Linux-based systems
+    ping -q -c 1 -w 1 8.8.8.8 > /dev/null 2>&1
+  fi
+}
+
 # Function to get the current branch name
 function git_current_branch() {
   git -C "$CONFIG_REPO_PATH" symbolic-ref --short HEAD 2>/dev/null
 }
 
-# Fetch the latest changes
-git -C "$CONFIG_REPO_PATH" fetch &>/dev/null
+# Check internet connectivity
+if check_internet; then
+  # Fetch the latest changes
+  git -C "$CONFIG_REPO_PATH" fetch &>/dev/null
 
-# Get the current branch name
-CURRENT_BRANCH=$(git_current_branch)
+  # Get the current branch name
+  CURRENT_BRANCH=$(git_current_branch)
 
-# Temporary merge to detect conflicts
-git -C "$CONFIG_REPO_PATH" checkout -b temp-merge-branch "${CURRENT_BRANCH}" &>/dev/null
-git -C "$CONFIG_REPO_PATH" merge "${MAIN_BRANCH}" --no-commit --no-ff &>/dev/null
+  # Temporary merge to detect conflicts
+  git -C "$CONFIG_REPO_PATH" checkout -b temp-merge-branch "${CURRENT_BRANCH}" &>/dev/null
+  git -C "$CONFIG_REPO_PATH" merge "${MAIN_BRANCH}" --no-commit --no-ff &>/dev/null
 
-# Count the number of conflicts
-CONFLICT_COUNT=$(git -C "$CONFIG_REPO_PATH" ls-files -u | wc -l)
+  # Count the number of conflicts
+  CONFLICT_COUNT=$(git -C "$CONFIG_REPO_PATH" ls-files -u | wc -l)
 
-# Clean up temporary branch
-git -C "$CONFIG_REPO_PATH" merge --abort &>/dev/null
-git -C "$CONFIG_REPO_PATH" checkout "${CURRENT_BRANCH}" &>/dev/null
-git -C "$CONFIG_REPO_PATH" branch -D temp-merge-branch &>/dev/null
+  # Clean up temporary branch
+  git -C "$CONFIG_REPO_PATH" merge --abort &>/dev/null
+  git -C "$CONFIG_REPO_PATH" checkout "${CURRENT_BRANCH}" &>/dev/null
+  git -C "$CONFIG_REPO_PATH" branch -D temp-merge-branch &>/dev/null
 
-# Display a warning message if the conflict count exceeds the threshold
-if [ "${CONFLICT_COUNT}" -gt "${CONFLICT_THRESHOLD}" ]; then
-  echo "Warning: There are ${CONFLICT_COUNT} potential merge conflicts between ${CURRENT_BRANCH} and ${MAIN_BRANCH} in the .config repository. Consider merging and resolving conflicts soon."
+  # Display a warning message if the conflict count exceeds the threshold
+  if [ "${CONFLICT_COUNT}" -gt "${CONFLICT_THRESHOLD}" ]; then
+    echo "Warning: There are ${CONFLICT_COUNT} potential merge conflicts between ${CURRENT_BRANCH} and ${MAIN_BRANCH} in the .config repository. Consider merging and resolving conflicts soon."
+  fi
 fi
