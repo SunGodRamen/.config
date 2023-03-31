01c648fd296f4b16dfefa6b4e30ccd6c21099a82
AVONSTU1 <AVONSTU1@ford.com>
2023-03-31 14:14:54 -0400

    silence git commands

--- zsh/check-git-conflicts.zsh
+++ zsh/check-git-conflicts.zsh
@@ -11,23 +11,22 @@ function git_current_branch() {
 }
 
 # Fetch the latest changes
-git -C "$CONFIG_REPO_PATH" fetch
+git -C "$CONFIG_REPO_PATH" fetch &>/dev/null
 
 # Get the current branch name
 CURRENT_BRANCH=$(git_current_branch)
 
 # Temporary merge to detect conflicts
-git -C "$CONFIG_REPO_PATH" checkout -b temp-merge-branch "${CURRENT_BRANCH}"
-git -C "$CONFIG_REPO_PATH" merge "${MAIN_BRANCH}" --no-commit --no-ff
+git -C "$CONFIG_REPO_PATH" checkout -b temp-merge-branch "${CURRENT_BRANCH}" &>/dev/null
+git -C "$CONFIG_REPO_PATH" merge "${MAIN_BRANCH}" --no-commit --no-ff &>/dev/null
 
 # Count the number of conflicts
 CONFLICT_COUNT=$(git -C "$CONFIG_REPO_PATH" ls-files -u | wc -l)
 
 # Clean up temporary branch
-git -C "$CONFIG_REPO_PATH" merge --abort
-git -C "$CONFIG_REPO_PATH" checkout "${CURRENT_BRANCH}"
-git -C "$CONFIG_REPO_PATH" branch -D temp-merge-branch
-clear
+git -C "$CONFIG_REPO_PATH" merge --abort &>/dev/null
+git -C "$CONFIG_REPO_PATH" checkout "${CURRENT_BRANCH}" &>/dev/null
+git -C "$CONFIG_REPO_PATH" branch -D temp-merge-branch &>/dev/null
 
 # Display a warning message if the conflict count exceeds the threshold
 if [ "${CONFLICT_COUNT}" -gt "${CONFLICT_THRESHOLD}" ]; then
