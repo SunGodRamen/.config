8388591ea968f19cfc2180c776effd246b1fe36f
AVONSTU1 <AVONSTU1@ford.com>
2023-03-31 14:08:56 -0400

    correct check .config directory

--- omz-custom/check-git-conflicts.zsh
+++ omz-custom/check-git-conflicts.zsh
@@ -1,34 +1,35 @@
 #!/bin/zsh
 
 # Variables
+CONFIG_REPO_PATH="$HOME/.config"
 MAIN_BRANCH="main"
 CONFLICT_THRESHOLD=5
 
 # Function to get the current branch name
 function git_current_branch() {
-  git symbolic-ref --short HEAD 2>/dev/null
+  git -C "$CONFIG_REPO_PATH" symbolic-ref --short HEAD 2>/dev/null
 }
 
 # Fetch the latest changes
-git fetch
+git -C "$CONFIG_REPO_PATH" fetch
 
 # Get the current branch name
 CURRENT_BRANCH=$(git_current_branch)
 
 # Temporary merge to detect conflicts
-git checkout -b temp-merge-branch "${CURRENT_BRANCH}"
-git merge "${MAIN_BRANCH}" --no-commit --no-ff
+git -C "$CONFIG_REPO_PATH" checkout -b temp-merge-branch "${CURRENT_BRANCH}"
+git -C "$CONFIG_REPO_PATH" merge "${MAIN_BRANCH}" --no-commit --no-ff
 
 # Count the number of conflicts
-CONFLICT_COUNT=$(git ls-files -u | wc -l)
+CONFLICT_COUNT=$(git -C "$CONFIG_REPO_PATH" ls-files -u | wc -l)
 
 # Clean up temporary branch
-git merge --abort
-git checkout "${CURRENT_BRANCH}"
-git branch -D temp-merge-branch
+git -C "$CONFIG_REPO_PATH" merge --abort
+git -C "$CONFIG_REPO_PATH" checkout "${CURRENT_BRANCH}"
+git -C "$CONFIG_REPO_PATH" branch -D temp-merge-branch
+clear
 
 # Display a warning message if the conflict count exceeds the threshold
 if [ "${CONFLICT_COUNT}" -gt "${CONFLICT_THRESHOLD}" ]; then
-  echo "Warning: There are ${CONFLICT_COUNT} potential merge conflicts between ${CURRENT_BRANCH} and ${MAIN_BRANCH}. Consider merging and resolving conflicts soon."
+  echo "Warning: There are ${CONFLICT_COUNT} potential merge conflicts between ${CURRENT_BRANCH} and ${MAIN_BRANCH} in the .config repository. Consider merging and resolving conflicts soon."
 fi
-
