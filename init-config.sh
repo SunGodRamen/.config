b90ec2fd79be524bfcc3fa9725d6c789312549a9
sungodra <avons394@gmail.com>
2023-11-03 11:23:11 -0400

    env var os

--- init-config.sh
+++ init-config.sh
@@ -7,14 +7,14 @@ export ENABLE_ZSH=true
 export ENABLE_OH_MY_ZSH=true
 export ENABLE_SDKMAN=true
 
-# initializations
-
+# set operating system : CURRENT_OS
 source $CONFIG/os-config
 
-# Neovim
-if [ "$ENABLE_NEOVIM" = true ]; then
-    export EDITOR='nvim'
-    alias v='nvim'
+# initializations
+
+# debian/ubuntu
+if [ "${CURRENT_OS:-}" = "DEBIAN_BASED" ]; then
+    source "$CONFIG/alias/debian"
 fi
 
 # bash
@@ -30,6 +30,12 @@ fi
 # oh-my-zsh
 if [ "$ENABLE_OH_MY_ZSH" = true ]; then
     source $CONFIG/oh-my-zsh/oh-my-zshrc
+fi
+
+# Neovim
+if [ "$ENABLE_NEOVIM" = true ]; then
+    export EDITOR='nvim'
+    alias v='nvim'
 fi
 
 # SDKMAN
