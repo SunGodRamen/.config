14e3cfadf1cecab4f44aa13ed4c8f45f8238cbd9
sungodra <avons394@gmail.com>
2023-11-03 10:56:51 -0400

    os config

--- init-config.sh
+++ init-config.sh
@@ -1,33 +1,40 @@
 export CONFIG=$HOME/.config
 
-# Neovim
+# configurable apps
 export ENABLE_NEOVIM=true
+export ENABLE_BASH=false
+export ENABLE_ZSH=true
+export ENABLE_OH_MY_ZSH=true
+export ENABLE_SDKMAN=true
+
+# initializations
+
+source $CONFIG/os-config
+
+# Neovim
 if [ "$ENABLE_NEOVIM" = true ]; then
     export EDITOR='nvim'
     alias v='nvim'
 fi
 
 # bash
-export ENABLE_BASH=false
 if [ "$ENABLE_BASH" = true ]; then
     source $CONFIG/bash/bashrc
 fi
 
 # zsh
-export ENABLE_ZSH=true
 if [ "$ENABLE_ZSH" = true ]; then
     source $CONFIG/zsh/zshrc
 fi
 
 # oh-my-zsh
-export ENABLE_OH_MY_ZSH=true
 if [ "$ENABLE_OH_MY_ZSH" = true ]; then
     source $CONFIG/oh-my-zsh/oh-my-zshrc
 fi
 
 # SDKMAN
-export ENABLE_SDKMAN=true
 if [ "$ENABLE_SDKMAN" = true ]; then
     export SDKMAN_DIR="$HOME/.sdkman"
     [[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
+    source $CONFIG/alias/sdkman
 fi
